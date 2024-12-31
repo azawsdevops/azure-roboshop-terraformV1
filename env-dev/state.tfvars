e2979947eb5e570397661340b77ab3e569c08971
kp3073 <keyurpatel8288@gmail.com>
2024-12-30 23:14:23 -0500

    roboshop terraform | vm module

---
+++ env-dev/state.tfvars
@@ -0,0 +1,5 @@
+
+resource_group_name  = "azuredevops"
+storage_account_name = "cloudawsaz"
+container_name       = "terraform-dev"
+key                  = "dev.tfstate"
---
+++ main.tf
@@ -0,0 +1,7 @@
+module "components" {
+  for_each  = var.component
+  source    = "./modules/vm"
+  component = each.value["name"]
+  vm_size   = "Standard_B2s"
+  env       = var.env
+}
