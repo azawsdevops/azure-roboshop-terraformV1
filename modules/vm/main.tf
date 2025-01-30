92aea2501a99a5bbbfb47db19e480a5796cdef1f
kp3073 <keyurpatel8288@gmail.com>
2025-01-30 12:52:39 -0500

    terrafrom | fetching secrects from Vault

--- modules/vm/main.tf
+++ modules/vm/main.tf
@@ -83,8 +83,8 @@ resource "azurerm_virtual_machine" "main" {
   }
   os_profile {
 	computer_name  = var.component
-	admin_username = data.vault_generic_secret.ssh.data["ssh_username"]
-	admin_password = data.vault_generic_secret.ssh.data["ssh_password"]
+	ssh_username = data.vault_generic_secret.ssh.data["ssh_username"]
+	ssh_password = data.vault_generic_secret.ssh.data["ssh_password"]
   }
   os_profile_linux_config {
 	disable_password_authentication = false
@@ -103,8 +103,8 @@ resource "null_resource" "ansible" {
 
 	connection {
 	  type     = "ssh"
-	  admin_username = data.vault_generic_secret.ssh.data["ssh_username"]
-	  admin_password = data.vault_generic_secret.ssh.data["ssh_password"]
+	  ssh_username = data.vault_generic_secret.ssh.data["ssh_username"]
+	  ssh_password = data.vault_generic_secret.ssh.data["ssh_password"]
 	  host     = azurerm_public_ip.main.ip_address
 	}
 
