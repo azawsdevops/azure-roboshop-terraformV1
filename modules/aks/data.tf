0d3c4dd78a34586d609e374518de4cf8dca9dcb7
kp3073 <keyurpatel8288@gmail.com>
2025-01-30 12:08:30 -0500

    terraform robosho aks module updated

---
+++ modules/aks/data.tf
@@ -0,0 +1,9 @@
+data "azurerm_resource_group" "main" {
+  name = "azuredevops"
+}
+
+data "azurerm_subnet" "main" {
+  name                 = "default"
+  virtual_network_name = "azure-network"
+  resource_group_name  = data.azurerm_resource_group.main.name
+}
---
+++ modules/aks/main.tf
@@ -0,0 +1,34 @@
+resource "azurerm_kubernetes_cluster" "main" {
+  name                = "roboshop-aks"
+  location            = data.azurerm_resource_group.main.location
+  resource_group_name = data.azurerm_resource_group.main.name
+  dns_prefix          = "dev"
+
+  default_node_pool {
+	name       = "default"
+	node_count = 1
+	vm_size    = "Standard_D2_v2"
+	enable_auto_scaling = true
+	min_count = 1
+	max_count = 5
+	vnet_subnet_id = "/subscriptions/4b236e6d-2c9a-4cb2-90a2-30a5377d8eb2/resourceGroups/azuredevops/providers/Microsoft.Network/virtualNetworks/azure-network/subnets/default"
+  }
+  aci_connector_linux {
+	subnet_name = "/subscriptions/4b236e6d-2c9a-4cb2-90a2-30a5377d8eb2/resourceGroups/azuredevops/providers/Microsoft.Network/virtualNetworks/azure-network/subnets/default"
+  }
+  network_profile {
+	network_plugin = "azure"
+	service_cidr   = "10.100.0.0/24"
+	dns_service_ip = "10.100.0.100"
+	
+  }
+  identity {
+	type = "SystemAssigned" 
+	
+  }
+
+  tags = {
+	Environment = "${var.env}-aks-roboshop"
+  }
+}
+
