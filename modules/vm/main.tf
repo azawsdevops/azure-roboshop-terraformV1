eac8a07e695783126fbf4eba1bd0719faa5167c5
kp3073 <keyurpatel8288@gmail.com>
2025-01-30 15:20:31 -0500

    terrafrom | fetching secrects from Vault

--- modules/vm/main.tf
+++ modules/vm/main.tf
@@ -112,7 +112,7 @@ resource "null_resource" "ansible" {
 	inline = [
 	  "sudo dnf install python3.12-pip -y",
 	  "sudo pip3.12 install ansible",
-	  "ansible-pull -i localhost, -U https://github.com/kp3073/az-roboshop-ansible roboshop.yml -e app_name=${var.component} -e env=${var.env}"
+	  "ansible-pull -i localhost, -U https://github.com/kp3073/az-roboshop-ansible roboshop.yml -e app_name=${var.component} -e ENV=${var.env}"
 	]
   }
 }
