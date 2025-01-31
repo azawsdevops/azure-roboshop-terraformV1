9b039f9aa66e5a75026a470e37c1b8eb6e64482f
kp3073 <keyurpatel8288@gmail.com>
2025-01-31 13:26:09 -0500

    terrafrom | active module AKS

--- main.tf
+++ main.tf
@@ -1,13 +1,13 @@
-module "databse" {
+module "database" {
   for_each  = var.database  # var.component is a map for all the components , if you want to deploy app with Terraform
-  source    = "./vm"
+  source    = "./modules/vm"
   component = each.value["name"]
   vm_size   = "Standard_B2s"
   env       = var.env
   vault_token = var.token
 }
 module "aks" {
-  source = "./aks"
+  source = "./modules/aks"
 }
 
 
