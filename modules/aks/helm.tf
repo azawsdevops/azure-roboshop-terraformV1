687d124523e5477b760c7bfb7f33bcf7273d67fe
kp3073 <keyurpatel8288@gmail.com>
2025-02-07 15:53:11 -0500

    AKS - helm | db jobs fixing

---
+++ modules/aks/helm.tf
@@ -0,0 +1,37 @@
+resource "null_resource" "kubeconfig" {
+  depends_on = [azurerm_kubernetes_cluster.main]
+  provisioner "local-exec" {
+	command = <<EOF
+	az aks get-credentials --resource-group ${data.azurerm_resource_group.main.name} --name main --overwrite-existing
+	EOF
+  }
+}
+
+
+resource "helm_release" "external-secrets" {
+  depends_on = [null_resource.kubeconfig]
+  name       = "external-secrets"
+  repository = "https://charts.external-secrets.io"
+  chart      = "external-secrets"
+  namespace  = "kube-system"
+}
+
+resource "null_resource" "external-secrets" {
+  depends_on = [helm_release.external-secrets]
+  provisioner "local-exec" {
+	command = <<EOF
+	kubectl apply -f /opt/vault-token.yaml
+	EOF
+  }
+}
+
+# resource "null_resource" "argocd" {
+#   depends_on = [null_resource.kubeconfig]
+#   provisioner "local-exec" {
+# 	command = <<EOF
+# kubectl apply -f ${path.module}/files/argocd-ns.yaml
+# kubectl apply -f ${path.module}/files/argocd.yaml -n argocd
+# EOF
+#   }
+# }
+# 
