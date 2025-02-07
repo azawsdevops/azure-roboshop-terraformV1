b5b1840db07f322004dd464b13e989c837fd9034
kp3073 <keyurpatel8288@gmail.com>
2025-02-07 16:08:34 -0500

    AKS - helm | db jobs fixing

--- modules/aks/helm.tf
+++ modules/aks/helm.tf
@@ -2,7 +2,7 @@ resource "null_resource" "kubeconfig" {
   depends_on = [azurerm_kubernetes_cluster.main]
   provisioner "local-exec" {
 	command = <<EOF
-	az aks get-credentials --resource-group ${data.azurerm_resource_group.main.name} --name main --overwrite-existing
+	az aks get-credentials --resource-group ${data.azurerm_resource_group.main.name} --name "roboshop-aks" --overwrite-existing
 	EOF
   }
 }
