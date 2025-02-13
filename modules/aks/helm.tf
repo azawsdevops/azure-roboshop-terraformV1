5541871a675be0c5b66a83e71a5cd68b9270dc11
kp3073 <keyurpatel8288@gmail.com>
2025-02-13 09:19:34 -0500

    Helm dev-destroy added

--- modules/aks/helm.tf
+++ modules/aks/helm.tf
@@ -30,7 +30,7 @@ resource "null_resource" "argocd" {
   provisioner "local-exec" {
 	command = <<EOF
 kubectl apply -f ${path.module}/files/argocd-ns.yaml
-kubectl apply -f ${path.module}/files/argocd.yaml -n argocd
+kubectl apply -f ${path.module}/files/argocd-config.yaml -n argocd
 EOF
   }
 }
