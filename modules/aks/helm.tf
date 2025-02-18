410d67a5c63601d95cded149f5f172141b3f5631
kp3073 <keyurpatel8288@gmail.com>
2025-02-18 13:11:29 -0500

    Kubectl installed

--- modules/aks/helm.tf
+++ modules/aks/helm.tf
@@ -20,7 +20,7 @@ resource "null_resource" "external-secrets" {
   depends_on = [helm_release.external-secrets]
   provisioner "local-exec" {
 	command = <<EOF
-	kubectl create secret generic vault_token --from-literal=token=${var.vault_token}
+    kubectl apply -f /opt/vault-token.yaml 
 	EOF
   }
 }
