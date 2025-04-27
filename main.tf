c9738e534e0bb404c81c0806e629b0a986d3fb58
kp3073 <keyurpatel8288@gmail.com>
2025-04-27 16:36:08 -0400

    frontend

--- main.tf
+++ main.tf
@@ -1,13 +1,13 @@
 module "database" {
   for_each  = var.database  # var.component is a map for all the components , if you want to deploy app with Terraform
-  source    = "modules/vm"
+  source    = "./modules/vm"
   database = each.value["name"]
   vm_size   = "Standard_B2s"
   env       = var.env
   vault_token = var.token
 }
 module "aks" {
-  source = "modules/aks"
+  source = "./modules/aks"
     env    = var.env
     vault_token = var.token
 }
