3b296ffd11772d26e4832a97cae5b024c083d743
kp3073 <keyurpatel8288@gmail.com>
2025-04-28 12:24:01 -0400

    ingress

--- modules/aks/main.tf
+++ modules/aks/main.tf
@@ -6,7 +6,7 @@ resource "azurerm_kubernetes_cluster" "main" {
 
   default_node_pool {
 	name       = "default"
-	node_count = 1
+# 	node_count = 1
 	vm_size    = "Standard_D2_v2"
 	auto_scaling_enabled = true
     min_count = 1
